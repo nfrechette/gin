4385a23e05a9d9a13d9d0ad4af0fbbe3e13d1672
Nicholas Frechette <zeno490@gmail.com>
2016-04-17 19:45:21 -0400

    Updating copyright notices for 2016.

--- include/gin/platforms.h
+++ include/gin/platforms.h
@@ -4,7 +4,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // The MIT License (MIT)
 //
-// Copyright (c) 2015 Nicholas Frechette
+// Copyright (c) 2015-2016 Nicholas Frechette
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
